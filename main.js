caed2701ca50988fec84e7f28624ae2454332e14
Saïd Benaissa <said@vivaldi.com>
2023-03-06 20:38:32 +0100

    Order of precedence is: New binding, Explicit binding, Implicit binding and Default binding.

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 // this, is the global object in the browser
 // and the global object in node js, that is the window object
 
+/* conclusion, the order of precedence is:
+    // 1. New binding
+    // 2. Explicit binding
+    // 3. Implicit binding
+    // 4. Default binding
+*/
+
 /*
 function getName(name) {
     console.log(`My name is ${name}`);
@@ -23,6 +30,9 @@ let person = {
 
 // 2. explicit binding, this is the object that is calling the method getName()
 // call
+
+global.name = 'Saïd';
+
 function getName() {
     console.log(`My name is ${this.name}`);
 }
@@ -32,12 +42,15 @@ function getName() {
 
 // 3. new binding, this is the object that is calling the method getName()
 function Person(name) {
-    // this = {};
+    // this = {}; // reference to the new object
     this.name = name;
 }
 let p1 = new Person('John'); // new keyword creates a new object and assign it to this object
 let p2 = new Person('Saïd');
-console.log(p1.name);
-console.log(p2.name);
+// console.log(p1.name, p2.name);
+
+
+// (4) default binding, this is the global object
+getName(); // (4), if non of the above rules apply, the default binding is the global object
 
 
